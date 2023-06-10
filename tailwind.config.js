ead423b86858f0f95f2d5b13bd2cacfa4b112236
R-INIYAVAN <iniyavanofficial21072002@gmail.com>
2023-06-10 10:50:13 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize:{
+        'very-small':'9px',
+        'very-very-small':'5px'
+      },
+
+      width:{
+        'hundred':'100px',
+        'dummy':'433px',
+        '150':'150px'
+      },
+      height:{
+        '40':'43px'
+      },
+      margin:{
+        '132':'132px',
+        '21':'21px',
+        '45':'45px'
+      }
+    },
+  },
+  plugins: [],
+}
+
